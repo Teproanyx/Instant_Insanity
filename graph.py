0b435b70599091f228de5a877be75048e50453c5
Teproanyx <poomgammer@gmail.com>
2023-02-24 23:50:56 +0700

    Refactor printing multigraph

--- graph.py
+++ graph.py
@@ -1,23 +1,28 @@
 from dice import Dice
 from typing import List
 import igraph as ig
-import matplotlib.pyplot as plt
 
 
 def graph_solution(die: List[Dice], colors: List[str]):
-    dice_id = [i for j in range(1, 5) for i in [j]*3]
-    edges = [d for dices in die for d in dices.to_edge()]
+    edges = [dices.to_edge() for dices in die]
 
-    g = ig.Graph(n=4, vertex_attrs={"name": colors})
-    g.add_edges(edges)
-    g.es['id'] = dice_id
+    g = [ig.Graph(n=4, vertex_attrs={"name": colors}) for i in range(len(edges))]
+    for graph, edge_list in zip(g, edges):
+        graph.add_edges(edge_list)
 
-    visual_style = {"layout": g.layout_grid(), "margin": 100, "edge_width": [1 + 2*(n-1) for n in dice_id],
+    mg = ig.Graph(n=4, vertex_attrs={"name": colors})
+    mg.add_edges([edge for each_edge in edges for edge in each_edge])
+
+    dice_id = [i for j in range(1, len(edges)) for i in [j] * 3]
+    mg.es['id'] = dice_id
+
+    print_multigraph(dice_id, mg)
+
+
+def print_multigraph(dice_id, mg):
+    visual_style = {"layout": mg.layout_grid(), "margin": 100, "edge_width": [1 + 2 * (n - 1) for n in dice_id],
                     "edge_color": [edge_to_color(e) for e in dice_id]}
-    # fig, ax = plt.subplots()
-    # ig.plot(g, target=ax, layout=layout, margin=200)
-    # plt.show()
-    ig.plot(g, target='test.svg', **visual_style)
+    ig.plot(mg, target='test.svg', **visual_style)
 
 
 def edge_to_color(edge: int) -> str:
