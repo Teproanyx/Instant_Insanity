b9755a012d2bdf4a917d7594dd2e1f80d22cda0f
Teproanyx <poomgammer@gmail.com>
2023-02-25 00:17:07 +0700

    Remove brute force from main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from dice import Dice
-import instant_insanity
 from graph import graph_solution
 
 
@@ -8,10 +7,6 @@ def main():
     # die = [Dice(list("YWRYBW")), Dice(list("RRBYWB")), Dice(list("RBBWYY")), Dice(list("WWRBWY"))]
 
     graph_solution(die, list("RWGB"))
-
-    # for dices in instant_insanity.brute_force(die):
-    #     print(dices)
-    # print(edge_solution(die))
 
 
 if __name__ == '__main__':
