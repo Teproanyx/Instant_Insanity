4084ab57c43fc367f47b5a37458c7fe19012bbf4
Teproanyx <poomgammer@gmail.com>
2023-02-25 12:16:21 +0700

    Extract dice list generation into function

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ from typing import List
 
 
 def main():
-    die = [Dice(list("RRWRGB")), Dice(list("RWWRBG")), Dice(list("BGBGRW")), Dice(list("GWWBRG"))]
-    # die = [Dice(list("YWRYBW")), Dice(list("RRBYWB")), Dice(list("RBBWYY")), Dice(list("WWRBWY"))]
+    dice_set_1 = four_dice_set("RRWRGB", "RWWRBG", "BGBGRW", "GWWBRG")
+    # dice_set_2 = four_dice_set("YWRYBW", "RRBYWB", "RBBWYY", "WWRBWY")
+    # impossible_set = four_dice_set("BGYRBR", "BYYYGR", "GGBRYB", "BYGRGR")
+
+    graph_solution(dice_set_1, get_color_dice_set(dice_set_1[0]))
+
+
+def four_dice_set(dice1: str, dice2: str, dice3: str, dice4: str) -> List[Dice]:
+    return [Dice(list(dice1)), Dice(list(dice2)), Dice(list(dice3)), Dice(list(dice4))]
 
 
 def get_color_dice_set(dice: Dice) -> List[str]:
