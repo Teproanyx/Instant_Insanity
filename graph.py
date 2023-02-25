7c8531ca10b7343bcc3c5a06cef07594954df40d
Teproanyx <poomgammer@gmail.com>
2023-02-25 12:17:45 +0700

    Generalized graph printing

--- graph.py
+++ graph.py
@@ -7,8 +7,9 @@ def graph_solution(die: List[Dice], colors: List[str]):
     edges = [dices.to_edge() for dices in die]
 
     g = [ig.Graph(n=4, vertex_attrs={"name": colors}) for i in range(len(edges))]
-    for graph, edge_list in zip(g, edges):
+    for i, (graph, edge_list) in enumerate(zip(g, edges)):
         graph.add_edges(edge_list)
+        graph.es['id'] = i + 1
 
     mg = ig.Graph(n=4, vertex_attrs={"name": colors})
     mg.add_edges([edge for each_edge in edges for edge in each_edge])
@@ -20,22 +21,20 @@ def graph_solution(die: List[Dice], colors: List[str]):
     print_multigraph(mg)
 
 
-def print_graph_list(graphs: List[ig.Graph], out_filename):
+
+def print_graph_list(graphs: List[ig.Graph], output: str):
     for i, graph in enumerate(graphs):
-        visual_style = {"layout": graph.layout_grid(), "margin": 50,
-                        "vertex_label": [color for color in graph.vs["name"]],
-                        "vertex_color": [vertex_name_to_color(v) for v in graph.vs["name"]]}
-        ig.plot(graph, target=out_filename + str(i + 1) + '.svg', **visual_style)
+        print_graph(graph, output + str(i + 1))
 
 
-def print_multigraph(multigraph):
-    visual_style = {"layout": multigraph.layout_grid(), "margin": 100,
-                    "vertex_label": [color for color in multigraph.vs["name"]],
-                    "vertex_color": [vertex_name_to_color(v) for v in multigraph.vs["name"]],
-                    "edge_label": [dice_id for dice_id in multigraph.es["id"]],
-                    "edge_width": [dice_id + 1 for dice_id in multigraph.es["id"]],
-                    "edge_color": [dice_id_to_color(dice_id) for dice_id in multigraph.es["id"]]}
-    ig.plot(multigraph, target='multigraph.svg', **visual_style)
+def print_graph(graph: ig.Graph, output: str):
+    visual_style = {"layout": graph.layout_grid(), "margin": 100,
+                    "vertex_label": [color for color in graph.vs["name"]],
+                    "vertex_color": [vertex_name_to_color(v) for v in graph.vs["name"]],
+                    "edge_label": [dice_id for dice_id in graph.es["id"]],
+                    "edge_width": [dice_id + 1 for dice_id in graph.es["id"]],
+                    "edge_color": [dice_id_to_color(dice_id) for dice_id in graph.es["id"]]}
+    ig.plot(graph, target=output + '.svg', **visual_style)
 
 
 def vertex_name_to_color(edge: str) -> str:
