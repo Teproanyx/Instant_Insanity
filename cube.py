19188a94cec84f29e5371fe4617d8e5fe4e0fd57
Teproanyx <poomgammer@gmail.com>
2023-02-27 17:19:39 +0700

    Hide cube turner method at the end of class

--- cube.py
+++ cube.py
@@ -10,18 +10,6 @@ class Cube:
         self.down = face[4]
         self.back = face[5]
 
-    def turn_left(self) -> None:
-        self.left, self.back, self.front, self.right = self.front, self.left, self.right, self.back
-
-    def turn_right(self) -> None:
-        self.right, self.front, self.back, self.left = self.front, self.left, self.right, self.back
-
-    def turn_up(self) -> None:
-        self.up, self.back, self.down, self.front = self.front, self.up, self.back, self.down
-
-    def turn_down(self) -> None:
-        self.down, self.front, self.up, self.back = self.front, self.up, self.back, self.down
-
     def __str__(self) -> str:
         return self.up + self.left + self.front + self.right + self.down + self.back
 
@@ -36,3 +24,15 @@ class Cube:
     def to_edgelist(self):
         """Turn Cube object into an edgelist for graphing"""
         return [(self.up, self.down), (self.front, self.back), (self.left, self.right)]
+
+    def turn_left(self) -> None:
+        self.left, self.back, self.front, self.right = self.front, self.left, self.right, self.back
+
+    def turn_right(self) -> None:
+        self.right, self.front, self.back, self.left = self.front, self.left, self.right, self.back
+
+    def turn_up(self) -> None:
+        self.up, self.back, self.down, self.front = self.front, self.up, self.back, self.down
+
+    def turn_down(self) -> None:
+        self.down, self.front, self.up, self.back = self.front, self.up, self.back, self.down
